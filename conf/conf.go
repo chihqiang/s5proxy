7aaf2c7d5e00a3ca71853518fc80f01f44afe5b0
zhiqiang <zhiqiang2033@gmail.com>
2025-09-30 15:14:48 +0800

    ♻️ refactor: Change the yaml tag to mapstructure to adapt to the new configuration parser

--- conf/conf.go
+++ conf/conf.go
@@ -1,8 +1,8 @@
 package conf
 
 type Config struct {
-	Listen       string            `yaml:"listen"`
-	MaxConns     int               `yaml:"max_conns"`
-	Users        map[string]string `yaml:"users"`
-	AllowedHosts []string          `yaml:"allowed_hosts"`
+	Listen       string            `mapstructure:"listen"`
+	MaxConns     int               `mapstructure:"max_conns"`
+	Users        map[string]string `mapstructure:"users"`
+	AllowedHosts []string          `mapstructure:"allowed_hosts"`
 }
